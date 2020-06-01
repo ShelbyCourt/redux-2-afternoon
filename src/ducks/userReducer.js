59eddb7057e94e1380145ee7f7e817feffbaf3cc
ShelbyCourt <shelby.b.courtney@gmail.com>
2020-06-01 14:21:39 -0700

    Through Step 4

---
+++ src/ducks/userReducer.js
@@ -0,0 +1,11 @@
+// import { userReducer } from "react"
+
+const initialState = {
+    email: null,
+    firstName: null,
+    lateName: null
+}
+
+export default function userReducer(state = initialState, action) {
+    return state;
+}
